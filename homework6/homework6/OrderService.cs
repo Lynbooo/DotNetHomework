aa410a6f90191e1efd3eae231f21f5458f64d64d
Lynbooo <73394472+Lynbooo@users.noreply.github.com>
2021-04-11 20:48:19 +0800

    homework6

---
+++ homework6/homework6/OrderService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace homework6
+{
+    public class OrderService
+    {
+        public List<Order> ordersList = new List<Order>();
+        public OrderService() { }
+        public bool AddOrder(Order newOrder)
+        {
+            foreach (Order order in ordersList)
+            {
+                if (order.Equals(newOrder))
+                    throw new ArgumentOutOfRangeException("invalid order");
+            }
+            ordersList.Add(newOrder);
+            ordersList.Sort((o1, o2) => Convert.ToInt32(o1.OrderId) - Convert.ToInt32(o2.OrderId));
+            return true;
+        }
+        public bool DeleteOrder(Order theOrder)
+        {
+            Order orderToDelete = new Order();
+            foreach (Order order in ordersList)
+            {
+                if (order.Equals(theOrder))
+                    orderToDelete = order;
+            }
+            ordersList.Remove(orderToDelete);
+            return true;
+        }
+        public Order QueryOrder(String id)
+        {
+            IEnumerable<Order> order = from o in ordersList
+                                       where o.OrderId == id
+                                       select o;
+            Order theOrder = new Order();
+            foreach (var o in order)
+                theOrder = o;
+            return theOrder;
+        }
+        public bool ModifyOrder(Order oldOrder, Order newOrder)
+        {
+            ordersList.Remove(oldOrder);
+            ordersList.Add(newOrder);
+            return true;
+        }
+
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Order>));
+        //根据xmlserializer来把对象序列化成XML，必须至少有一个参数为空的构造函数
+        public void Export(String filePath) //将所有order序列化为XML文件
+        {
+            if (Path.GetExtension(filePath) != ".xml") throw new ArgumentException("文件类型错误");
+            using (FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                xmlSerializer.Serialize(fs, this.ordersList);
+            }
+        }
+        public void Import(String filePath) //从XML文件中载入订单
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read))
+            {
+                if (Path.GetExtension(filePath) != ".xml") throw new ArgumentException("文件类型错误");
+                ordersList.Clear(); //清空ordersList集合中所有的元素对象
+                if(fs.Length != 0)
+                    this.ordersList = (List<Order>)xmlSerializer.Deserialize(fs);
+            }
+        }
+    }
+}
---
+++ homework6/homework6Tests/OrderServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework6;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.IO;
+
+namespace homework6.Tests
+{
+    [TestClass()]
+    public class OrderServiceTests
+    {
+        OrderService administrator = new OrderService();
+        Order orderForTest = new Order(new Client("张三", "15529993888", "WHU"), "20214130", 0.8, DateTime.Now, new OrderDetails("QQ糖", 5, 6));
+        Order orderForTest2 = new Order(new Client("李四", "15529993888", "WHU"), "20214130", 0.8, DateTime.Now, new OrderDetails("QQ糖", 5, 6));
+        OrderDetails detailForTest = new OrderDetails("biscuit", 8, 3);
+        [TestInitialize]
+        public void Init()
+        {
+            administrator.AddOrder(orderForTest);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ApplicationException))]
+        public void AddOrderTest()
+        {
+            //检查重复
+            administrator.AddOrder(new Order(new Client("张三", "15529993888", "WHU"), "20214130", 0.8, DateTime.Now, new OrderDetails("QQ糖", 5, 6)));
+        }
+
+        [TestMethod()]
+        public void DeleteOrderTest()
+        {
+            Assert.AreEqual(true, administrator.DeleteOrder(orderForTest));
+        }
+
+        [TestMethod()]
+        public void QueryOrderTest()
+        {
+            Assert.AreEqual(orderForTest, administrator.QueryOrder("20214130"));
+        }
+
+        [TestMethod()]
+        public void ModifyOrderTest()
+        {
+            Assert.AreNotEqual(orderForTest, administrator.ModifyOrder(orderForTest, orderForTest2));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExportTest()
+        {
+            administrator.Export("test.txt");
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            administrator.Export("test.xml");
+            administrator.ordersList.Clear();
+            administrator.Import("test.xml");
+            Assert.AreEqual("20214130", administrator.ordersList.First().OrderId);
+        }
+        [TestCleanup]
+        public void Clean()
+        {
+            File.Delete("test.xml");
+            File.Delete("test.txt");
+            File.Create("test.xml");
+            File.Create("test.txt");
+        }
+    }
+}
